3ba432ef5b076b2f19de0a052783523cc75f69d7
mohammad <mohammadrhmany132@gmail.com>
2025-12-12 22:20:31 +0330

    i created card componenet

--- admin-panel/src/components/card/card.js
+++ admin-panel/src/components/card/card.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import './card.css'
+import React from "react";
+import "./card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className='card'>
-        <div className='card-icon'>
-            <div className='icon'></div>
-            <div className='card-info'> </div>
+    <div className="card">
+      <div className="card-icon">
+        <div className="icon">
+          {props.icon}
         </div>
-        <div className='card-data'></div>
+        <div className="card-info">
+          <p className="card-title">{props.title}</p>
+          <h5 className="percent">{props.description}</h5>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- admin-panel/src/pages/home/home.js
+++ admin-panel/src/pages/home/home.js
@@ -4,6 +4,32 @@ import Header from "../../components/header/Header";
 import Card from "../../components/card/card";
 
 export default function Home() {
+  const cardArrauInfo = [
+    {
+      id: 1,
+      title: "کاربر های جدید",
+      icon: <i class="bi bi-person-add"></i>,
+      description: "+52",
+    },
+    {
+      id: 2,
+      title: "فروش هفتگی",
+      icon: <i class="bi bi-shop-window"></i>,
+      description: "-15%",
+    },
+    {
+      id: 3,
+      title: "تمامی کابران",
+      icon: <i class="bi bi-people"></i>,
+      description: "300",
+    },
+    {
+      id: 4,
+      title: "در آمد هفتگی",
+      icon: <i class="bi bi-bank"></i>,
+      description: "+23%",
+    },
+  ];
   return (
     <div className="home">
       <div className="home-side-bar">
@@ -14,10 +40,9 @@ export default function Home() {
           <Header />
         </header>
         <section className="cards">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {cardArrauInfo.map((cardData) => (
+            <Card {...cardData} key={cardData.id}/>
+          ))}
         </section>
       </div>
     </div>
